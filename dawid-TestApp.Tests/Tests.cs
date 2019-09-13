a1a6a79cac1ccd5c237728f26c910fd2313f211e
Dawid Szczepaniak <dawid.szczepaniak@gmail.com>
2019-09-13 12:36:01 +0200

    Changed test project

---
+++ dawid-TestApp.Tests/Tests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dawid_TestApp.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
