a19b1b5875d103224e3f487d59fe94e039f93cfa
Dawid Szczepaniak <dawid.szczepaniak@gmail.com>
2019-09-13 13:54:57 +0200

    Test error

--- dawid-TestApp.Tests/Tests.cs
+++ dawid-TestApp.Tests/Tests.cs
@@ -8,7 +8,7 @@ namespace dawid_TestApp.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(true);
+            Assert.IsTrue(false);
         }
     }
 }
