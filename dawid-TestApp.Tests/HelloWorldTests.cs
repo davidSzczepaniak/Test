5ddb6945670b5ce74b9370b9c61496be49e846e3
Dawid Szczepaniak <dawid.szczepaniak@gmail.com>
2019-09-13 10:47:28 +0200

    Added tests project

---
+++ dawid-TestApp.Tests/HelloWorldTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace dawid_TestApp.Tests
+{
+    public class HelloWorldTests
+    {
+        [Fact]
+        public void NewTest()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
