a79e7d597d4493ef92d6052ab7845985cc9d2772
Dawid Szczepaniak <dawid.szczepaniak@gmail.com>
2019-09-16 10:56:34 +0200

    dddd

--- dawid_TestApp/Controllers/ValuesController.cs
+++ dawid_TestApp/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace dawid_TestApp.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "TEST1", "tEST2" };
         }
 
         // GET api/values/5
