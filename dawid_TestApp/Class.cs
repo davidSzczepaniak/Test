654a14b2d28653651549447970bcb13ad0d4b491
Dawid Szczepaniak <dawid.szczepaniak@pcmicorp.com>
2020-07-14 21:47:04 +0200

    local commit

---
+++ dawid_TestApp/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dawid_TestApp
+{
+    public class Class
+    {
+        public int TESTrepoBranch { get; set; }
+    }
+}
